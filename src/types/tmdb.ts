5a50166c9f565abc0a66f4334321319b7b2c0912
ChristianDedios23 <Chrisdedios73@gmail.com>
2026-04-16 00:03:04 -0700

    Created get movie details endpoint

--- src/types/tmdb.ts
+++ src/types/tmdb.ts
@@ -4,10 +4,21 @@ export type TMDBMovie = {
   poster_path: string | null;
   release_date: string;
   overview: string;
+  id: number;
 };
 
 //Stores list of movie objects
 export type TMDBResponse = {
   results: TMDBMovie[];
-  message?: string;
+};
+
+//need to add genre, languages and creator
+export type TMDBMovieDetailed = {
+  title: string;
+  poster_path: string | null;
+  release_date: string;
+  overview: string;
+  revenue: number;
+  runtime: number;
+  budget: number;
 };
