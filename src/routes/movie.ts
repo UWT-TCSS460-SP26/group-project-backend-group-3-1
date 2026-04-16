5777e35a786b1bad7fbdfed4fd4ccc67675f3b1e
Christian <Chrisdedios73@gmail.com>
2026-04-16 00:34:50 -0700

    Fix lint and test issues

--- src/routes/movie.ts
+++ src/routes/movie.ts
@@ -1,4 +1,4 @@
-import { Router, Request, Response } from 'express';
+import { Router } from 'express';
 import { getMovieDetails, getPopularMovies, searchMovies } from '../controllers/movie';
 
 const movieRouter = Router();
