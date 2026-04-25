e24afbdc257a6414d98e85880f43aefeb01d1e78
kevlam1 <kevinlam1970@gmail.com>
2026-04-25 15:02:20 -0700

    fixed migration issues?

--- prisma/migrations/20260425220031_add_reviews_ratings/migration.sql
+++ prisma/migrations/20260425220031_add_reviews_ratings/migration.sql
@@ -1,36 +1,35 @@
 /*
   Warnings:
 
+  - The primary key for the `Rating` table will be changed. If it partially fails, the table could be left without primary key constraint.
   - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
   - You are about to drop the column `UserID` on the `User` table. All the data in the column will be lost.
   - You are about to drop the `reviews` table. If the table is not empty, all the data it contains will be lost.
-  - Added the required column `email` to the `User` table without a default value. This is not possible if the table is not empty.
   - The required column `id` was added to the `User` table with a prisma-level default value. This is not possible if the table is not empty. Please add this column as optional, then populate it before making it required.
-  - Added the required column `username` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Made the column `email` on table `User` required. This step will fail if there are existing NULL values in that column.
 
 */
+-- DropForeignKey
+ALTER TABLE "Rating" DROP CONSTRAINT "Rating_userId_fkey";
+
 -- DropForeignKey
 ALTER TABLE "reviews" DROP CONSTRAINT "reviews_UserID_fkey";
 
 -- AlterTable
+ALTER TABLE "Rating" DROP CONSTRAINT "Rating_pkey",
+ALTER COLUMN "userId" SET DATA TYPE TEXT,
+ADD CONSTRAINT "Rating_pkey" PRIMARY KEY ("ratingId", "userId");
+
+-- AlterTable
 ALTER TABLE "User" DROP CONSTRAINT "User_pkey",
 DROP COLUMN "UserID",
-ADD COLUMN     "email" TEXT NOT NULL,
 ADD COLUMN     "id" TEXT NOT NULL,
-ADD COLUMN     "username" VARCHAR(20) NOT NULL,
+ALTER COLUMN "email" SET NOT NULL,
+ALTER COLUMN "username" DROP DEFAULT,
 ADD CONSTRAINT "User_pkey" PRIMARY KEY ("id");
 
 -- DropTable
 DROP TABLE "reviews";
-
--- CreateTable
-CREATE TABLE "Rating" (
-    "ratingId" SERIAL NOT NULL,
-    "userId" TEXT NOT NULL,
-    "movieShow" BOOLEAN NOT NULL,
-
-    CONSTRAINT "Rating_pkey" PRIMARY KEY ("ratingId","userId")
-);
 
 -- CreateTable
 CREATE TABLE "Review" (
